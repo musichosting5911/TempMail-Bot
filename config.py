8671aa8f48d2b646aa795e90276d36a5251620e5
musichosting5911 <tgid251@gmail.com>
2025-11-05 12:53:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "23579732"  # Replace this API ID with your actual API ID
+API_HASH = "ad766ada11094a308a689c79ff173b25"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8232277531:AAHOzo5xCdT6oHZtxOU8qMi1nIfqK-J95zY"  # Replace this BOT_TOKEN
